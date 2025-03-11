92c0f4f37888a59addc9a0d7409ab8e31667b798
Karoki <wanjikukar19@gmail.com>
2025-03-11 14:02:38 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(){
+    let a = 2000;
+    let b = 100;
+    let result = a/b;
+    console.log("result");
+    return result;
+}
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
